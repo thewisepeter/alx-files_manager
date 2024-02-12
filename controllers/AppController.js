18a68c8e872248765bfac7ccca7c965ff08e697e
thewisepeter <rempeterwisdom@gmail.com>
2024-02-12 19:08:27 +0300

    implemetation of server.js

---
+++ controllers/AppController.js
@@ -0,0 +1,28 @@
+const redisClient = require('../utils/redis');
+const dbClient = require('../utils/db');
+
+class AppController {
+    static async getStatus(req, res) {
+        try {
+            const redisStatus = redisClient.isAlive();
+            const dbStatus = dbClient.isAlive();
+
+            res.status(200).json({ redis: redisStatus, db: dbStatus });
+    } catch (error) {
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  }
+
+  static async getStats(req, res) {
+    try {
+      const numUsers = await dbClient.nbUsers();
+      const numFiles = await dbClient.nbFiles();
+
+      res.status(200).json({ users: numUsers, files: numFiles });
+    } catch (error) {
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  }
+}
+
+module.exports = AppController;
